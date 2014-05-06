020771009981e501df7937607d0e8190e2438855
brianengineer <126039@gmail.com>
2014-05-06 21:22:35 +0200

    added final verstion of delay sketch9 - PBI 39 now onto highrep

---
+++ Catch21/Odroid_Code/Operation_Modes/High_Repetition/Sketch9_Delay_Final/main.cpp
@@ -0,0 +1,40 @@
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
+//!! Based on the code example from http://opencv-srf.blogspot.no/2011/09/capturing-images-videos.html !!
+
+
+int main()
+{
+    cv::VideoCapture cap(0); // open the video camera no. 0
+    cv::Vector <cv::Mat> imgBuf (900);
+    int i=0;
+    int j=0;
+
+    cv::namedWindow("Buffer",CV_WINDOW_AUTOSIZE); // create a window
+    cv::Mat frame;
+    while (1)
+    {
+
+        // read a new frame from video
+
+        cap >> frame;
+        frame.copyTo(imgBuf[i]); // store images from camera in buffer
+        i++;
+
+        if(i > 30) //
+        {
+            cv::imshow("Buffer", imgBuf[j]); // show the frame in "Buffer" window
+            j++;
+        }
+
+
+          if (cv::waitKey(1) == 27) // wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+          {
+             std::cout << "esc key is pressed by user" << std::endl;
+             break;
+          }
+     } //end while
+          return 0;
+
+} // end main
