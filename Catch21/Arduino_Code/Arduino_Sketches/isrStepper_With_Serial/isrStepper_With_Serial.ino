48149d67319cace4a2d13ff4190aab01d8c26f36
Even Hørtvedt <even@kvenna.net>
2014-05-08 11:04:30 +0200

    cleaned arduino code

--- Catch21/Arduino_Code/Arduino_Sketches/isrStepper_With_Serial/isrStepper_With_Serial.ino
+++ Catch21/Arduino_Code/Arduino_Sketches/isrStepper_With_Serial/isrStepper_With_Serial.ino
@@ -1,13 +1,14 @@
+#include <HMsTimer2.h>
 #include <StepperMod.h>
-#include <MsTimer2.h>
+
 
 // Motor
-int in1Pin = 11;
-int in2Pin = 10;
-int in3Pin = 9;
-int in4Pin = 8;
-int in5Pin = 5;
-int in6Pin = 6;
+int in1Pin = 8;
+int in2Pin = 7;
+int in3Pin = 6;
+int in4Pin = 5;
+int in5Pin = 10;
+int in6Pin = 9;
 StepperMod motor(200, in1Pin, in2Pin, in3Pin, in4Pin, in5Pin, in6Pin);
 
 // Other
@@ -17,12 +18,21 @@ String temp;
 int rpm= 1;
 int directionInt = 0;
 int stepDelay = 0;
-long nextStep = 0;
+int pwm = 255;
+
+// Led debug
+int ledPos = 3;
+int ledNeg = 4;
 
 void setup() {
   Serial.begin(9600);
-  MsTimer2::set(1, isr); // 500ms period
-  MsTimer2::start();
+  //Change pwm frequency to 31250hz by not using prescaler
+  TCCR1B |= (1<<CS20);
+  TCCR1B &= ~((1<<CS22) | (1<<CS21));
+  
+  // Debug leds
+  pinMode(ledPos, OUTPUT);
+  pinMode(ledNeg, OUTPUT);
 }
 
 void loop() {
@@ -33,13 +43,77 @@ void loop() {
         stringIn += charIn;
         if (charIn == '.')
         {
+          // Disables interrupts for this section
+          noInterrupts();
+          
+          // Decodes recived info
           temp = stringIn.substring(0, 2);
           directionInt = temp.toInt();
           temp = stringIn.substring(2, 5);
           rpm = temp.toInt();
-          stepDelay = 300 / rpm;
+          
+          //Do not divide by zero!
+          if (rpm <= 0)
+          {
+            rpm=1;
+            if(directionInt == 0)
+            {
+              rpm = 600;
+            }
+          }
+          
+          //Set the delay
+          stepDelay = 6000 / rpm;
           stringIn = "";
-          nextStep = millis() + stepDelay;
+          
+          //Set the pwm
+          
+          //50% duty
+          if (rpm < 100)
+            pwm = 128;
+            
+          //60% duty
+          else if (rpm < 200)
+            pwm = 153;
+            
+          //70% duty
+          else if (rpm < 300)
+            pwm = 179;
+            
+          //100% duty
+          else
+            pwm = 255;
+          
+          // Setup and start the timer might include a if clause and stop the timer if directionInt == 0
+          HMsTimer2::stop();
+          HMsTimer2::set(stepDelay, isr);
+          HMsTimer2::start();
+          
+          // Led debug
+          if (directionInt > 0)
+          {
+            digitalWrite(ledPos, HIGH);
+            digitalWrite(ledNeg, LOW);
+          }
+          
+          else if (directionInt <= -1)
+          {
+            digitalWrite(ledPos, LOW);            
+            digitalWrite(ledNeg, HIGH);
+          }
+           
+          else if (directionInt == 0)
+          {
+            digitalWrite(ledPos, HIGH);            
+            digitalWrite(ledNeg, HIGH);
+          }
+          else
+          {
+            digitalWrite(ledPos, LOW);            
+            digitalWrite(ledNeg, LOW);
+          }
+        // enables interrupts
+          interrupts();        
         }
       }
   
@@ -47,9 +121,10 @@ void loop() {
 
 void isr() 
 {
-  if (directionInt != 0 && millis() > nextStep)
+  noInterrupts();
+  if (directionInt != 0)
   {
-    nextStep = millis() + stepDelay;
+    motor.setPwm(pwm);
     motor.step(directionInt);
   }
   
@@ -57,4 +132,5 @@ void isr()
   {
     motor.enablersOff();
   }
+  interrupts(); 
 }
