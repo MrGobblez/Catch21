c4165e83788c3bc9df6628a15a14ba70f762eda6
Even Hørtvedt <even@kvenna.net>
2014-05-25 22:33:38 +0200

    Added latest Arduino Sketches

--- Catch21/Arduino_Code/Arduino_Sketches/isrStepper_With_Serial/isrStepper_With_Serial.ino
+++ Catch21/Arduino_Code/Arduino_Sketches/isrStepper_With_Serial/isrStepper_With_Serial.ino
@@ -2,23 +2,28 @@
 #include <StepperMod.h>
 
 
-// Motor
+// Motor pins
 int in1Pin = 8;
 int in2Pin = 7;
 int in3Pin = 6;
 int in4Pin = 5;
-int in5Pin = 10;
-int in6Pin = 9;
-StepperMod motor(200, in1Pin, in2Pin, in3Pin, in4Pin, in5Pin, in6Pin);
+int enablerA = 10;
+int enablerB = 9;
 
-// Other
-char charIn;
-String stringIn;
-String temp;
+
+// Motor control
 int rpm= 1;
 int directionInt = 0;
 int stepDelay = 0;
 int pwm = 255;
+
+StepperMod motor(200, in1Pin, in2Pin, in3Pin, in4Pin, enablerA, enablerB);
+
+// For communication
+char charIn;
+String stringIn;
+String temp;
+
 
 // Led debug
 int ledPos = 3;
@@ -46,71 +51,80 @@ void loop() {
           // Disables interrupts for this section
           noInterrupts();
           
-          // Decodes recived info
-          temp = stringIn.substring(0, 2);
-          directionInt = temp.toInt();
-          temp = stringIn.substring(2, 5);
-          rpm = temp.toInt();
-          
-          //Do not divide by zero!
-          if (rpm <= 0)
+          // Checks for ID request
+          if (stringIn == "12345.")
           {
-            rpm=1;
-            if(directionInt == 0)
+            Serial.println("C");
+            stringIn = "";
+          }
+          else
+          {
+            // Decodes recived info
+            temp = stringIn.substring(0, 2);
+            directionInt = temp.toInt();
+            temp = stringIn.substring(2, 5);
+            rpm = temp.toInt();
+          
+            //Do not divide by zero!
+            if (rpm <= 0)
             {
-              rpm = 600;
+              rpm=1;
+              if(directionInt == 0)
+              {
+                rpm = 600;
+              }
             }
-          }
-          
-          //Set the delay
-          stepDelay = 6000 / rpm;
-          stringIn = "";
-          
-          //Set the pwm
-          
-          //50% duty
-          if (rpm < 100)
-            pwm = 128;
             
-          //60% duty
-          else if (rpm < 200)
-            pwm = 153;
+            //Set the delay
+            stepDelay = 6000 / rpm; // change 6000 to constant
+            stringIn = "";
             
-          //70% duty
-          else if (rpm < 300)
-            pwm = 179;
+            //Set the pwm
             
-          //100% duty
-          else
-            pwm = 255;
-          
-          // Setup and start the timer might include a if clause and stop the timer if directionInt == 0
-          HMsTimer2::stop();
-          HMsTimer2::set(stepDelay, isr);
-          HMsTimer2::start();
-          
-          // Led debug
-          if (directionInt > 0)
-          {
-            digitalWrite(ledPos, HIGH);
-            digitalWrite(ledNeg, LOW);
-          }
-          
-          else if (directionInt <= -1)
-          {
-            digitalWrite(ledPos, LOW);            
-            digitalWrite(ledNeg, HIGH);
-          }
-           
-          else if (directionInt == 0)
-          {
-            digitalWrite(ledPos, HIGH);            
-            digitalWrite(ledNeg, HIGH);
-          }
-          else
-          {
-            digitalWrite(ledPos, LOW);            
-            digitalWrite(ledNeg, LOW);
+            //50% duty
+            if (rpm < 100)
+              pwm = 128;
+              
+            //60% duty
+            else if (rpm < 200)
+              pwm = 153;
+              
+            //70% duty
+            else if (rpm < 300)
+              pwm = 179;
+              
+            //100% duty
+            else
+              pwm = 255;
+            
+            // Setup and start the timer might include a if clause and stop the timer if directionInt == 0
+            HMsTimer2::stop();
+            HMsTimer2::set(stepDelay, isr);
+            HMsTimer2::start();
+            
+            // Led debug
+            if (directionInt > 0)
+            {
+              digitalWrite(ledPos, HIGH);
+              digitalWrite(ledNeg, LOW);
+            }
+            
+            else if (directionInt <= -1)
+            {
+              digitalWrite(ledPos, LOW);            
+              digitalWrite(ledNeg, HIGH);
+            }
+             
+            else if (directionInt == 0)
+            {
+              digitalWrite(ledPos, HIGH);            
+              digitalWrite(ledNeg, HIGH);
+            }
+            else
+            {
+              digitalWrite(ledPos, LOW);            
+              digitalWrite(ledNeg, LOW);
+            }
           }
         // enables interrupts
           interrupts();        
